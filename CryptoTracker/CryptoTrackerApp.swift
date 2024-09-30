95040bc4a2a35dc0769debd6c3fe70862f32294c
Marcin Wawer <marcinwaw_git@icloud.com>
2024-09-30 17:25:42 +0200

    initial commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Marcin Wawer on 30-09-2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
