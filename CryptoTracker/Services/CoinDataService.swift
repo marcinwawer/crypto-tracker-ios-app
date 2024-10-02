5064b215b89501e9e9a50cdac14c53f579fae67f
Marcin Wawer <marcinwaw_git@icloud.com>
2024-10-02 20:32:12 +0200

    [feature] add CoinDataService and NetworkingManager

---
+++ CryptoTracker/Services/CoinDataService.swift
@@ -0,0 +1,30 @@
+//
+//  CoinDataService.swift
+//  CryptoTracker
+//
+//  Created by Marcin Wawer on 02-10-2024.
+//
+
+import Foundation
+import Combine
+
+class CoinDataService {
+    @Published var allCoins: [CoinModel] = []
+    var coinSubscription: AnyCancellable?
+    
+    init () {
+        getCoins()
+    }
+    
+    private func getCoins () {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h&x_cg_demo_api_key=CG-ua76UZJbusWpdUyMpADNEvuo")
+                else { return }
+        
+        coinSubscription = NetworkingManager.download(url: url)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
+            })
+    }
+}
