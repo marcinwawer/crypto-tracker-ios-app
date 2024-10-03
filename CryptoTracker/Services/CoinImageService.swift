3475ee4c6ae3e34ad805b0a88c8231e2b055ed8b
Marcin Wawer <marcinwaw_git@icloud.com>
2024-10-03 14:09:51 +0200

    [feature] add file manager to save downloaded images and retrieve them

--- CryptoTracker/Services/CoinImageService.swift
+++ CryptoTracker/Services/CoinImageService.swift
@@ -14,23 +14,44 @@ class CoinImageService {
     
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
-        guard let url = URL(string: coin.image)
-        else { return }
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("retrieved image from file manager")
+        } else {
+            downloadCoinImage()
+            print("downloading image now")
+        }
+    }
+    
+    private func downloadCoinImage() {
+        guard let url = URL(string: coin.image) else { return }
         
         imageSubscription = NetworkingManager.download(url: url)
             .tryMap({ data in
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedImage in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard
+                    let self = self,
+                    let downloadedImage = returnedImage
+                else {
+                    return
+                }
+                
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
