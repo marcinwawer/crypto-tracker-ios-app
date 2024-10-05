ec97b18bf4bd3d58bf4a555d1fcd2c67c57a6736
Marcin Wawer <marcinwaw_git@icloud.com>
2024-10-05 18:11:20 +0200

    [F] add statistic view to home view

--- CryptoTracker/Extensions/DeveloperPreview.swift
+++ CryptoTracker/Extensions/DeveloperPreview.swift
@@ -13,6 +13,10 @@ class DeveloperPreview {
     
     let homeVM = HomeViewModel()
     
+    let stat1 = StatisticModel(title: "market cap", value: "$12.5Bn", percentageChange: 25.34)
+    let stat2 = StatisticModel(title: "total volume", value: "$1.23Tr")
+    let stat3 = StatisticModel(title: "portfolio value", value: "$50.4k", percentageChange: -12.34)
+    
     let coin = CoinModel(
         id: "bitcoin",
         symbol: "btc",
