ee66647ad9cd61e635a98669c9b4c0af87fb29cf
Marcin Wawer <marcinwaw_git@icloud.com>
2024-10-05 22:08:17 +0200

    [F] add market data

---
+++ CryptoTracker/Core/Home/Views/PortfolioView.swift
@@ -0,0 +1,18 @@
+//
+//  PortfolioView.swift
+//  CryptoTracker
+//
+//  Created by Marcin Wawer on 05-10-2024.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text("PORTFOLIO VIEW")
+    }
+}
+
+#Preview {
+    PortfolioView()
+}
