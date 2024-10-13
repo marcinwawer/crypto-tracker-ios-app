6fac49ca91ac4b277f50ba7e5ac5bfa85d779cc2
Marcin Wawer <marcinwaw_git@icloud.com>
2024-10-13 21:17:57 +0200

    feature: add settings view

--- CryptoTracker/Core/Components/XMarkButton.swift
+++ CryptoTracker/Core/Components/XMarkButton.swift
@@ -8,15 +8,9 @@
 import SwiftUI
 
 struct XMarkButton: View {
-    @Environment(\.presentationMode) var presentationMode
-    
     var body: some View {
-        Button {
-            presentationMode.wrappedValue.dismiss()
-        } label: {
-            Image(systemName: "xmark")
-                .font(.headline)
-        }
+        Image(systemName: "xmark")
+            .font(.headline)
     }
 }
 
---
+++ CryptoTracker/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,120 @@
+//
+//  SettingsView.swift
+//  CryptoTracker
+//
+//  Created by Marcin Wawer on 13-10-2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    let defaultURL = URL(string: "https://www.google.com")!
+    let youtubeURL = URL(string: "https://www.youtube.com/swiftfulthinking")!
+    let coffeeURL = URL(string: "https://www.buymeacoffee.com/nicksarno")!
+    let coingeckoURL = URL(string: "https://www.coingecko.com")!
+    let personalURL = URL(string: "https://www.github.com/marcinwawer")!
+    
+    var body: some View {
+        NavigationView {
+            List {
+                swiftfulThinkingSection
+                coinGeckoSection
+                developerSection
+                applicationSection
+            }
+            .tint(.blue)
+            .navigationTitle("Settings")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    XMarkButton().onTapGesture { 
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
+
+extension SettingsView {
+    private var swiftfulThinkingSection: some View {
+        Section {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Text("This app was made by following a @SwiftfulThinking course on Youtube. It uses MVVM architecture, Combine and CoreData!")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundStyle(Color.theme.accent)
+            }
+            .padding(.vertical)
+            
+            Link("Subscribe on Youtube 🥳", destination: youtubeURL)
+            Link("Support his coffee addiction ☕️", destination: coffeeURL)
+        } header: {
+            Text("Swiftful Thinking")
+        }
+    }
+    
+    private var coinGeckoSection: some View {
+        Section {
+            VStack(alignment: .leading) {
+                Image("coingecko")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Text("The cryptocurrency data that is used in this app comes from a free API form CoinGecko! Prices may be slightly delayed.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundStyle(Color.theme.accent)
+            }
+            .padding(.vertical)
+            
+            Link("Visit CoinGecko 🦎", destination: coingeckoURL)
+        } header: {
+            Text("CoinGecko")
+        }
+    }
+    
+    private var developerSection: some View {
+        Section {
+            VStack(alignment: .leading) {
+                Image(systemName: "iphone.sizes")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Text("This app was developed by Marcin Wawer. It uses SwiftUI and is written 100% in Swift. The project benefits from mulit-threading, publishers/subscribers, and data persistance.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundStyle(Color.theme.accent)
+            }
+            .padding(.vertical)
+            
+            Link("Visit Github 🤝", destination: coingeckoURL)
+        } header: {
+            Text("Developer")
+        }
+    }
+    
+    private var applicationSection: some View {
+        Section {
+            Link("Terms of Service", destination: defaultURL)
+            Link("Pricay Policy", destination: defaultURL)
+            Link("Company Website", destination: defaultURL)
+            Link("Learn More", destination: defaultURL)
+        } header: {
+            Text("Application")
+        }
+    }
+}
