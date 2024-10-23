33cafa64ea68254d37c87824b3afb934d3a5ee0b
Marcin Wawer <marcinwaw_git@icloud.com>
2024-10-23 21:07:52 +0200

    feature: change animation time in launch view

--- CryptoTracker/Core/Launch/Views/LaunchView.swift
+++ CryptoTracker/Core/Launch/Views/LaunchView.swift
@@ -51,7 +51,7 @@ struct LaunchView: View {
                 if counter == lastIndex {
                     counter = 0
                     loops += 1
-                    if loops >= 2 {
+                    if loops >= 1 {
                         showLaunchView = false
                     }
                 } else {
