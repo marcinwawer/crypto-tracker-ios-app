3084a56753b5c4c5a72092f4cf75572781f0bdcc
Marcin Wawer <marcinwaw_git@icloud.com>
2024-10-23 21:43:47 +0200

    [clean] do some optimization with downloading data

--- CryptoTracker/Services/CoinImageService.swift
+++ CryptoTracker/Services/CoinImageService.swift
@@ -41,6 +41,7 @@ class CoinImageService {
             .tryMap({ data in
                 return UIImage(data: data)
             })
+            .receive(on: DispatchQueue.main)
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedImage in
                 guard
                     let self = self,
